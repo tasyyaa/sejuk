19ef6f99a3c7d658446df80cf1e329ae7c094264
Nafisa <nafisa102003@gmail.com>
2023-05-20 23:17:18 +0700

    fix: margin in another add address view

--- resources/views/addaddress.blade.php
+++ resources/views/addaddress.blade.php
@@ -15,7 +15,7 @@
         {{ csrf_field() }}
         <div class="flex flex-col gap-2 font-semibold text-[#497174]">
             <label for="custaddress_name">Address Name</label>
-            <select name="custaddress_name" class="h-8 rounded-sm" id="custaddress_name">
+            <select name="custaddress_name" class="h-8 rounded-sm px-3" id="custaddress_name">
                 <option value="" disabled selected>Select your option</option>
                 <option value="Home">Home</option>
                 <option value="Work">Work</option>
@@ -23,19 +23,19 @@
         </div>
         <div class="flex flex-col gap-2 font-semibold text-[#497174]">
             <label for="cust_address">Address</label>
-            <input type="text" class="h-8 rounded-sm" name="cust_address" required="required">
+            <input type="text" class="h-8 rounded-sm px-3" name="cust_address" required="required">
         </div >
         <div class="flex flex-col gap-2 font-semibold text-[#497174]">
             <label for="cust_city">City</label>
-            <input type="text" class="h-8 rounded-sm" name="cust_city" required="required">
+            <input type="text" class="h-8 rounded-sm px-3" name="cust_city" required="required">
         </div >
         <div class="flex flex-col gap-2 font-semibold text-[#497174]">
             <label for="cust_province">Province</label>
-            <input type="text" class="h-8 rounded-sm" name="cust_province" required="required">
+            <input type="text" class="h-8 rounded-sm px-3" name="cust_province" required="required">
         </div>
         <div class="flex flex-col gap-2 font-semibold text-[#497174]">
             <label for="cust_phonenumber">Telephone</label>
-            <input type="text" class="h-8 rounded-sm" name="cust_phonenumber" required="required">
+            <input type="text" class="h-8 rounded-sm px-3" name="cust_phonenumber" required="required">
         </div>
         <div class=" flex justify-center">
         <input type="Submit" class="w-[492px] h-[50px] bg-[#497174] text-white font-semibold rounded-lg" value="Add to List Addreess +">
