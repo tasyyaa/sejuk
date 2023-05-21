1b691d60e14fa05385faf3937182f9abd735907e
unknown <nisasalvia96@gmail.com>
2023-05-22 00:06:22 +0700

    nambahin register+login pake laravel brezee ver.1

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,6 @@
 <?php
 
-use App\Models\addaddress;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CheckController;
-use App\Http\Controllers\addCostumeController;
-use App\Http\Controllers\acceptPaymentController;
-use App\Http\Controllers\returnPackageController;
-use App\Http\Controllers\returnPaymentController;
-use App\Http\Controllers\testingHelperController;
-use App\Http\Controllers\applyForReturnController;
-
-
 
 /*
 |--------------------------------------------------------------------------
@@ -27,49 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::controller(addCostumeController::class)->group(function(){
-    Route::get('/vendorCatalog', 'index');
-    Route::get('/vendorCatalog/addCostume','create');
-    Route::post('/vendorCatalog/store','store');
-    Route::get('/vendorCatalog/delete/{id}','hapus');
-    Route::get('/vendorCatalog/addCostumeInvalid','view');
-    // Route::get('/pegawai/cari','cari');
-    // Route::get('/pegawai/view/{id}','view');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::controller(returnPackageController::class)->group(function(){
-    // Route::get('/ordersummary', 'index');
-    Route::get('/returnpackage','create');
-    Route::post('/returnpackage/store','store');
-    Route::get('/returnpackagecomplete', 'complete');
-    Route::get('/orderSummary', 'view');
-    Route::get('/orderSummaryConfirmed', 'viewconf');
-});
-
-Route::controller(applyForReturnController::class)->group(function(){
-    // Route::get('/ordersummary', 'index');
-    Route::get('/applyforreturn','create');
-    Route::post('/applyforreturn/store','store');
-    Route::get('/applyforreturncomplete', 'complete');
-});
-
-Route::controller(acceptPaymentController::class)->group(function(){
-    // Route::get('/ordersummary', 'index');
-    Route::get('/acceptpayment','create');
-    Route::post('/acceptpayment/store','store');
-    Route::get('/returncomplete', 'complete');
-});
-
-Route::controller(returnPaymentController::class)->group(function(){
-    // Route::get('/ordersummary', 'index');
-    Route::get('/returnpayment','create');
-    Route::post('/returnpayment/store','store');
-    Route::get('/returncomplete', 'complete');
-});
-
-Route::controller(CheckController::class)->group(function(){
-    // Route::get('/ordersummary', 'index');
-    Route::get('/addaddress','create');
-    Route::post('/addaddress/store','store');
-    Route::get('/address', 'complete');
-});
+require __DIR__.'/auth.php';
