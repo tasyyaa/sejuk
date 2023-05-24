9fdf5ce9171ea8110c5fd10ae348b254095e6650
Tasya Putri Aliya <5026211116@mhs.its.ac.id>
2023-05-24 09:22:33 +0700

    login bener

--- app/Http/Controllers/chooseController.php
+++ app/Http/Controllers/chooseController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class chooseController extends Controller
 {
+    // public function __construct(){
+    //     $this->middleware('Auth');
+    // }
+
     public function index()
     {
     	// mengambil data dari table pegawai
