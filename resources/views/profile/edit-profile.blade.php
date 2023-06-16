b0c361c284ac38705d2064d259cf52dfd8c22375
indiraamhrn <indiraamhrn@indiraamhrns-MacBook-Pro.local>
2023-06-16 09:21:23 +0700

    fix: invalid

--- resources/views/profile/edit-profile.blade.php
+++ resources/views/profile/edit-profile.blade.php
@@ -4,26 +4,30 @@
     Edit Profile
 @endsection
 
+
 @section('content')
-    <form action="{{route('profile.edit')}}" method="POST"
-          class="flex flex-col text-[#565656] justify-center items-start w-full max-w-6xl bg-white rounded-lg mx-auto my-20 pb-12">
+    <form action="{{ route('profile.edit') }}" method="POST"
+        class="flex flex-col text-[#565656] justify-center items-start w-full max-w-6xl bg-white rounded-lg mx-auto my-20 pb-12">
         @csrf
         <!-- Invalid -->
-{{--        <div x-show="invalid" class="fixed top-0 left-0 right-0 h-36 bg-white z-50" x-transition x-cloak>--}}
-{{--            <div class="flex flex-col px-12 pt-4 pb-2">--}}
-{{--                <h2 class="text-3xl font-bold text-[#FE4A4A]">--}}
-{{--                    Data is Invalid--}}
-{{--                </h2>--}}
-{{--                <p class="text-lg font-medium">Click OK to continue</p>--}}
-{{--                <p class="ml-auto mt-4 px-8 py-0.5 border-2 shadow-xl border-b-4 border-b-slate-300 hove:brightness-75 cursor-pointer"--}}
-{{--                   @click="invalid=false">--}}
-{{--                    Ok--}}
-{{--                </p>--}}
-{{--            </div>--}}
-{{--        </div>--}}
+        {{--        <div x-show="invalid" class="fixed top-0 left-0 right-0 h-36 bg-white z-50" x-transition x-cloak> --}}
+        {{--            <div class="flex flex-col px-12 pt-4 pb-2"> --}}
+        {{--                <h2 class="text-3xl font-bold text-[#FE4A4A]"> --}}
+        {{--                    Data is Invalid --}}
+        {{--                </h2> --}}
+        {{--                <p class="text-lg font-medium">Click OK to continue</p> --}}
+        {{--                <p class="ml-auto mt-4 px-8 py-0.5 border-2 shadow-xl border-b-4 border-b-slate-300 hove:brightness-75 cursor-pointer" --}}
+        {{--                   @click="invalid=false"> --}}
+        {{--                    Ok --}}
+        {{--                </p> --}}
+        {{--            </div> --}}
+        {{--        </div> --}}
         <!-- Invalid -->
-        @if($errors->any())
-            {{$errors}}
+        @if ($errors->any())
+            <button
+                class="font-bold text-3xl text-[#FE4A4A] px-12 py-1 bg-[#FFFFFF] mx-auto mt-5 border-2 shadow-xl border-b-4 border-b-slate-300">
+                Data Is Invalid
+            </button>
         @endif
 
         <div class="flex flex-col w-full pr-36 p-6">
@@ -47,28 +51,30 @@
                 <div class="flex flex-col gap-3 text-xl">
                     <div class="flex flex-row items-center">
                         <label for="fullname" class="w-48 font-semibold">Full Name</label>
-                        <input type="text" name="name" id="fullname" value="{{ old('name') ? old('name') : $user->name }}"
-                               class="border-[1px] border-black px-2 w-96 text-lg" required
-                                />
+                        <input type="text" name="name" id="fullname"
+                            value="{{ old('name') ? old('name') : $user->name }}"
+                            class="border-[1px] border-black px-2 w-96 text-lg" required />
                         <img src="/images/edit-logo-light.png" alt="edit-logo" class="w-4 ml-4" />
                     </div>
                     <div class="flex flex-row items-center">
                         <label for="email" class="w-48 font-semibold">Email</label>
                         <input type="email" name="email" id="email" placeholder="Customerid@gmail.com"
-                               class="border-[1px] border-black px-2 w-96 text-lg"
-                               required value="{{old('email') ?? $user->email}}" />
+                            class="border-[1px] border-black px-2 w-96 text-lg" required
+                            value="{{ old('email') ?? $user->email }}" />
                         <img src="/images/edit-logo-light.png" alt="edit-logo" class="w-4 ml-4" />
                     </div>
                     <div class="flex flex-row items-center">
                         <label for="phone" class="w-48 font-semibold">Phone Number</label>
                         <input type="text" name="phone_number" id="phone" placeholder="62812778348"
-                               class="border-[1px] border-black px-2 w-96 text-lg" required value="{{old('phone_number') ?? $user->phone_number}}" />
+                            class="border-[1px] border-black px-2 w-96 text-lg" required
+                            value="{{ old('phone_number') ?? $user->phone_number }}" />
                         <img src="/images/edit-logo-light.png" alt="edit-logo" class="w-4 ml-4" />
                     </div>
                     <div class="flex flex-row items-center">
                         <label for="phone" class="w-48 font-semibold">City</label>
                         <input type="text" name="city" id="phone" placeholder="Surabaya"
-                               class="border-[1px] border-black px-2 w-96 text-lg" required value="{{old('city') ?? $user->city}}" />
+                            class="border-[1px] border-black px-2 w-96 text-lg" required
+                            value="{{ old('city') ?? $user->city }}" />
                         <img src="/images/edit-logo-light.png" alt="edit-logo" class="w-4 ml-4" />
                     </div>
                 </div>
@@ -91,15 +97,13 @@
                             </p>
                         </div>
                         <div class="flex flex-row text-lg pr-10 items-center">
-                            <input type="text" name="cust_Creditcardtype1" id="mastercard"
-                                   placeholder="************7778"
-                                   maxlength="20"
-                                   value="{{ old('cust_Creditcardtype1') ?? $user->cust_Creditcardtype1 }}"
-                                   class="border-[1px] border-black px-2 w-48 text-lg" />
+                            <input type="text" name="cust_Creditcardtype1" id="mastercard" placeholder="************7778"
+                                maxlength="20" value="{{ old('cust_Creditcardtype1') ?? $user->cust_Creditcardtype1 }}"
+                                class="border-[1px] border-black px-2 w-48 text-lg" />
                             <img src="/images/edit-logo-light.png" alt="edit-logo" class="w-4 ml-4" />
-{{--                            <button class="underline font-light ml-12">--}}
-{{--                                Done--}}
-{{--                            </button>--}}
+                            {{--                            <button class="underline font-light ml-12"> --}}
+                            {{--                                Done --}}
+                            {{--                            </button> --}}
                         </div>
                     </div>
                     <div class="flex flex-row text-lg items-center justify-between w-full">
@@ -108,15 +112,13 @@
                             <label for="visa" class="ml-10">Visa</label>
                         </div>
                         <div class="flex flex-row text-lg pr-10 items-center">
-                            <input type="text" name="cust_Creditcardtype2" id="visa"
-                                   placeholder="************1010"
-                                   maxlength="20"
-                                   value="{{old('cust_Creditcardtype2') ?? $user->cust_Creditcardtype2}}"
-                                   class="border-[1px] border-black px-2 w-48 text-lg" />
+                            <input type="text" name="cust_Creditcardtype2" id="visa" placeholder="************1010"
+                                maxlength="20" value="{{ old('cust_Creditcardtype2') ?? $user->cust_Creditcardtype2 }}"
+                                class="border-[1px] border-black px-2 w-48 text-lg" />
                             <img src="/images/edit-logo-light.png" alt="edit-logo" class="w-4 ml-4" />
-{{--                            <button class="underline font-light ml-12">--}}
-{{--                                Done--}}
-{{--                            </button>--}}
+                            {{--                            <button class="underline font-light ml-12"> --}}
+                            {{--                                Done --}}
+                            {{--                            </button> --}}
                         </div>
                     </div>
                 </div>
@@ -127,26 +129,27 @@
                 <h2 class="text-3xl font-semibold">Address</h2>
                 <div class="flex flex-row items-center">
                     <input type="text" name="cust_homephone" id="homephone" placeholder="6281277346724"
-                           class="border-[1px] border-black px-2 w-[550px] text-lg" value="{{old('cust_homephone') ?? $user->cust_homephone}}" />
+                        class="border-[1px] border-black px-2 w-[550px] text-lg"
+                        value="{{ old('cust_homephone') ?? $user->cust_homephone }}" />
                     <img src="/images/edit-logo-light.png" alt="edit-logo" class="w-4 ml-4" />
                 </div>
                 <div class="flex flex-row items-center">
                     <input type="text" name="cust_address" id="address"
-                           value="{{old('cust_address') ?? $user->cust_address}}"
-                           placeholder="Jalan Teknik Sipil, Keputih, Sukolilo, Surabaya, Jawa Timur, 60111"
-                           class="border-[1px] border-black px-2 w-[550px] text-lg" />
+                        value="{{ old('cust_address') ?? $user->cust_address }}"
+                        placeholder="Jalan Teknik Sipil, Keputih, Sukolilo, Surabaya, Jawa Timur, 60111"
+                        class="border-[1px] border-black px-2 w-[550px] text-lg" />
                     <img src="/images/edit-logo-light.png" alt="edit-logo" class="w-4 ml-4" />
                 </div>
-{{--                <button class="flex flex-row items-center pt-4">--}}
-{{--                    <img src="/images/add-logo.png" alt="add-logo" class="w-8" />--}}
-{{--                    <p class="font-semibold ml-3">Add More</p>--}}
-{{--                </button>--}}
+                {{--                <button class="flex flex-row items-center pt-4"> --}}
+                {{--                    <img src="/images/add-logo.png" alt="add-logo" class="w-8" /> --}}
+                {{--                    <p class="font-semibold ml-3">Add More</p> --}}
+                {{--                </button> --}}
             </div>
         </div>
         <!-- Address Data -->
 
         <button type="submit"
-                class="font-bold text-xl uppercase text-white px-12 py-1 bg-[#497174] mx-auto mt-20 hover:brightness-75">
+            class="font-bold text-xl uppercase text-white px-12 py-1 bg-[#497174] mx-auto mt-20 hover:brightness-75">
             Update
         </button>
     </form>
