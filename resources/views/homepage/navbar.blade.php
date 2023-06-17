eef371197caf6092b4f2f46b4c4547e7cbc773a1
indiraamhrn <indiraamhrn@indiraamhrns-MacBook-Pro.local>
2023-06-17 21:30:05 +0700

    fix: navbar2

--- resources/views/homepage/navbar.blade.php
+++ resources/views/homepage/navbar.blade.php
@@ -230,9 +230,9 @@
                             </a>
                         </div>
                     @else
-                        <div class="absolute top-6 right-12">
-                            <a href="{{route('choose-login')}}" class="text-xl font-medium text-black">LOG IN</a>
-                            <a href="{{route('choose-register')}}" class="text-xl font-medium text-white bg-[#3C9379] rounded-lg py-2 px-8 mr-5">REGISTER</a>
+                        <div class="absolute top-6 right-12 py-3 px-4">
+                            <a href="{{route('choose-login')}}" class="text-xl font-medium text-black ml- ">LOG IN</a>
+                            <a href="{{route('choose-register')}}"  class="text-xl font-medium text-white bg-[#3C9379] rounded-lg py-2 px-8 mr-5 ml-[20px]">REGISTER</a>
                         </div>
                     @endif
 
