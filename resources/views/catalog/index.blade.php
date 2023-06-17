c63a8da8f7ea2ec2451d452a4c6eb9b6f72cac5f
afandarofan <afanda.rofan@gmail.com>
2023-06-17 17:40:55 +0700

    Rapiin GUI catalog index.blade

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -19,49 +19,66 @@
             <div class="flex flex-col ml-[99px]">
                 <img  src="{{ asset('storage/' . $product->catalog_image) }}" class="mt-[80px] w-[550px] h-[700px]">
             </div>
-            <div class="flex flex-col rounded-sm bg-[#F9F5F2] w-[500px] h-[460px] ml-[45px] mt-[200px]">
-                <div class="flex flex-row ml-[25px] mt-[32px]">
-                    <p class="d-inline-block  font-bold text-[#050505] text-[46px]">{{ $product->item_name }}</p>
+            <div class="flex flex-col rounded-sm bg-[#F9F5F2] w-[500px] h-[480px] ml-[45px] mt-[200px]">
+                <div class="flex flex-row ml-[25px] mt-[25px]">
+                    <h1 class="text-5xl pt-[36px] mb-[20px] whitespace-nowrap text-[#497174] font-bold">{{ $product->item_name }}</h1>
                 </div>
                 <div class="flex flex-row ml-[25px]">
                     <p class="d-inline-block  font-semibold text-[#B7B7B7] text-[24px]">{{ $product->vendor->vendor_store }}</p>
                 </div>
-                <div class="flex flex-row ml-[25px]">
-                    <div class="flex flex-col">
-                        <div class="flex flex-row">
-                            <p class="d-inline-block  font-semibold text-[#B7B7B7] text-[24px]">Size-</p>
-                            <p class="d-inline-block  font-bold text-[#050505] text-[24px]">{{ $product->size }}</p>
-                        </div>
-                        <div class="flex flex-row">
-                            <p class="d-inline-block  font-semibold text-[#B7B7B7] text-[24px]">Price-</p>
-                            <p class="d-inline-block  font-bold text-[#050505] text-[24px]">Rp{{ $product->item_price }}</p>
-                        </div>
-                        <div class="flex flex-row">
-                            <p class="d-inline-block  font-semibold text-[#B7B7B7] text-[24px]">Stok-</p>
-                            <p class="d-inline-block  font-bold text-[#050505] text-[24px]">{{ $product->stock }}</p>
-                        </div>
-                    </div>
-                    <div class="flex flex-col ml-[150px]">
-                        <a href="#" class="text-[#9B9B9B] font-semibold w-[196px] h-[51px] text-[18px]">(Size Guide)</a>
-                    </div>
-                </div>
+                <div class="ml-[20px]">
+
+                <table class= "border-separate border-spacing-2 border-slate-400 font-bold text-xl">
+                    <tbody class="pl-20">
+                        <tr >
+                            <td >Price</td>
+                            <td>:</td>
+                            <td class="d-inline-block  font-bold text-[#050505] text-[24px]" >Rp.{{ $product->item_price }} <!-- <-Harga Dummy;Harga sesuai database--></td>
+                        </tr>
+                        <tr>
+                            <td > Stok</td>
+                            <td>:</td>
+                            <td class="d-inline-block  font-bold text-[#050505] text-[24px]">{{ $product->stock }} <!-- <-Harga Dummy;Harga sesuai database--></td>
+                        </tr>
+                        <tr>
+                            <td >Size Guide</td>
+                            <td>:</td>
+                            <td class="d-inline-block  font-bold text-[#050505] text-[24px]" >{{ $product->size }}<!-- <-Ukuran Dummy;Ukuran sesuai database--></td>
+                        </tr>
+
+                        <tr>
+                            <td >Order Quantity</td>
+                            <td>:</td>
+
+
+
+                        </tr>
+                    </tbody>
+
+
+                </table>
+
+
+            </div>
                 <form action="{{route('cart.add')}}" class="" method="post">
                     @csrf
-                    <div class="flex flex-row ml-[15px]">
+                    <div class="flex flex-row ml-[20px]">
                         <input name="catalog_id" value="{{$product->catalog_id}}" hidden required/>
-                        <input class="rounded-md h-[33px] focus:ring-0 focus:border-transparent bg-[#F9F5F2] border-none form-control" type="number" id="quantity"
+                        <input class="rounded-md h-[33px] focus:ring-0 focus:border-transparent   form-control" type="number" id="quantity"
                                aria-labelledby="required" name="amount" required min="0" placeholder="Enter your quantity"/>
 
-                </div>
-                <div class="flex flex-row ml-[25px] mt-[20px]">
+                    </div>
+
+                <div class="flex flex-row ml-[25px] mt-[25px]">
                     <img src="{{ asset('storage/' . $product->catalog_image) }}"  width="100px" height="150px">
                 </div>
                 <div class="flex flex-row ml-[40px] mt-[15px] mb-[15px] justify-center">
-                    <input type="submit" class="bg-[#497174] text-[#d6e4e5] w-[196px] h-[51px] font-semibold rounded-md"
-                           value="Add to cart" />
+                    <button type="submit" class=" text-white btn bg-[#497174] hover:bg-[#497174]-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"><i class="fa fa-shopping-cart"></i>
+                        Add To Cart</button>
                 </div>
                 </form>
             </div>
         </div>
     </main>
+
 @endsection
